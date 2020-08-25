a6a18cdb0935142de5223a089d3bac110afd3584
chiselko6 <alex.limontov@gmail.com>
2020-08-25 20:52:56 +0300

    Replay mode: replay the whole game and stop at any event (#25)

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,3 +1,3 @@
 poetry run isort
-poetry run flake8 scrabble/ tests/ app.py
-poetry run mypy --ignore-missing-imports scrabble/ tests/ app.py
+poetry run flake8 scrabble/ tests/ app.py run_cmd.py
+poetry run mypy --ignore-missing-imports scrabble/ tests/ app.py run_cmd.py
